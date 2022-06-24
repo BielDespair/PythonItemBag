eed8543ab5533857bf509d54f476572825851463
Gabriel Leonel <105954239+BielDespair@users.noreply.github.com>
2022-06-24 06:14:01 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,7 +12,7 @@ class Bag(Operador):
             index_vazio = self.retorna_index_vazio()
             self.items[index_vazio] = item
             self.representacao()
-            print(f"Guardou {item} | Peso: {item.peso}")
+            print(f"Guardou {item.nome} | Peso: {item.peso}")
 
         elif(not slotfree):
             print("SEM ESPAÇO")
