aa32df5cdaff1ba56d12d58984d26a17ea219f18
Gabriel Leonel <105954239+BielDespair@users.noreply.github.com>
2022-06-24 05:44:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sacola = Operador(10,60)
 guardar = sacola.guardar_item
 sacola.guardar_item(lista["Espada"])
 
-sacola.representação()
+sacola.representacao()
 guardar = sacola.guardar_item
 remover = sacola.remover_item
 
