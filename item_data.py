81ffdbad6af90430df104da59a434c16fc2a97a0
Gabriel Leonel <105954239+BielDespair@users.noreply.github.com>
2022-06-24 06:04:31 -0300

    Update item_data.py

--- item_data.py
+++ item_data.py
@@ -6,11 +6,6 @@ class Item:
     def __str__(self):
         return f'{self.nome} Peso: {self.peso}'
 
-lista = {"Espada":Item("Espada",10),
-        "Armadura":Item("Armadura",20),
-        "Lixo":Item("Lixo",1),
-        "Adaga":Item("Adaga",3)}
-
 espada = Item("Espada",10)
 armadura = Item ("Armadura",20)
 adaga = Item ("Adaga",3)
