edd7d0090c370a3620759ad73200a26168f1b241
Gabriel Leonel <105954239+BielDespair@users.noreply.github.com>
2022-06-24 05:17:45 -0300

    Update BagItens.py

--- BagItens.py
+++ BagItens.py
@@ -1,11 +1,3 @@
-#insert para deixar um slot vazio na representação
-#representação de slots (como no jogo da forca talvez?)
-#classe item com peso,ocupações de slot,nome
-#classe bag com slots,capacidade de peso,etc (lista?)
-#def soma na classe item (para decidir a soma para comparação)
-# "x" for lenght of slots da bag 
-#slots = 4 dividir a classe em 4 partes para ocupar 4 slots
-
 from List_functions import Operador
 from Items import lista
 
